fbf66bba2b5e20475cd9bc65ceeb2c102cac081b
ethnic style <92246504+mijos52@users.noreply.github.com>
2021-10-20 16:50:51 +0000

    # using beautiful soup

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+from bs4 import BeautifulSoup as bs
 import requests
 from pprint import pprint
 
-x = requests.get('https://pokeapi.co/api/v2/generation/?limit=2')
-pprint(x.json())
+
+url_list = ['https://www.youtube.com','https://www.google.com','https://www.bing.com']
+
+def links(Url):
+  response = requests.get(Url)
+  html = response.content
+
+  #provide the response and the parser
+  soup = bs(html , 'lxml')
+
+  #Get all the links in a webpage 
+  '''
+  for each item in object collection of all tags with a 
+  print href in each a tag
+
+  '''
+  for link in soup.find_all('a'):
+    print(link.get('href'))
+
+# go through the list and fetch all the links in the page
+for i in url_list:
+   links(i)
